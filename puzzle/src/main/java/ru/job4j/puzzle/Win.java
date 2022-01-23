3138ecf95cea556d95aae66c6b6861f87c8f4843
marat346 <dela346@mail.ru>
2022-01-23 15:16:52 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,13 +2,15 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean result = false;
+        boolean rsl = false;
         for (int index = 0; index < board.length; index++) {
-            if ((board[1][index] != 'X') && (board[index][1] != 'X')) {
-                result = true;
+            if (board[index][0] == 1 && (board[index][1] == 1) && board[index][2] == 1 && board[index][3] == 1 && board[index][4] == 1 ||
+                    board[0][index] == 1 && board[1][index] == 1 && board[2][index] == 1 && board[3][index] == 1 && board[4][index] == 1) {
+                rsl = true;
                 break;
             }
         }
-        return result;
+        return rsl;
     }
 }
+
